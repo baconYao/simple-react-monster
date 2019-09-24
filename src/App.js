29fca81838bb50d1434ba550c5690d1dce52e40d
baconYao <bacon735392@gmail.com>
2019-09-24 20:44:10 +0800

    Divide search to independent component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import { CardList } from './components/card-list/card-list.component';
+import { SearchBox } from './components/search-box/search-box.component';
 
 class App extends Component {
   constructor() {
@@ -25,14 +26,13 @@ class App extends Component {
     );
 
     return (
-    <div className="App">
-      <input type="search" placeholder="search monsters" onChange={ e => {
-        this.setState({searchField: e.target.value}, () => {
-          console.log(this.state);
-        })}
-      }/>
-      <CardList monsters={ filteredMonsters } />
-    </div>
+      <div className="App">
+        <SearchBox 
+          placeholder="search monsters" 
+          handleChange={ e => this.setState({ searchField: e.target.value }) }  
+        />
+        <CardList monsters={ filteredMonsters } />
+      </div>
     );
   }
 }
