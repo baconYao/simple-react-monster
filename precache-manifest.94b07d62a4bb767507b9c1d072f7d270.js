b365e8376bb5148a0689857e2b7cf0d4edee5eb9
baconYao <bacon735392@gmail.com>
2019-09-24 21:13:23 +0800

    Updates

---
+++ precache-manifest.94b07d62a4bb767507b9c1d072f7d270.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7256cf7791fa314a662ff6db409c65ab",
+    "url": "/simple-react-monster/index.html"
+  },
+  {
+    "revision": "21714efe7556ebac9fd1",
+    "url": "/simple-react-monster/static/css/main.c32cfdce.chunk.css"
+  },
+  {
+    "revision": "30103b5533dd9228877c",
+    "url": "/simple-react-monster/static/js/2.6fce9c4b.chunk.js"
+  },
+  {
+    "revision": "21714efe7556ebac9fd1",
+    "url": "/simple-react-monster/static/js/main.ee7a82dc.chunk.js"
+  },
+  {
+    "revision": "dc25a768460fad8ae60f",
+    "url": "/simple-react-monster/static/js/runtime-main.fa23d399.js"
+  }
+]);
