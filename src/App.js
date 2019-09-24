00c4a6bf68debdbbf519ac66085ffc65d64c2203
baconYao <bacon735392@gmail.com>
2019-09-24 20:20:58 +0800

    Implement search functionality

--- src/App.js
+++ src/App.js
@@ -17,9 +17,13 @@ class App extends Component {
       .then(response => response.json())
       .then(users => this.setState({monsters: users}))
   }
-  
 
   render() {
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter(monster => 
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+    );
+
     return (
     <div className="App">
       <input type="search" placeholder="search monsters" onChange={ e => {
@@ -27,7 +31,7 @@ class App extends Component {
           console.log(this.state);
         })}
       }/>
-      <CardList monsters={this.state.monsters} />
+      <CardList monsters={ filteredMonsters } />
     </div>
     );
   }
